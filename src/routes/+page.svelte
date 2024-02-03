bf712843ce3caca1482644b0142734a1a023aeda
seamus-giess <seamusgiess@gmail.com>
2024-02-04 00:41:03 +1000

    feat: reworked to use sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="container p-5">
+  <h1>welcome to rot-dot-bot</h1>
+</div>
+
+<style lang="scss">
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
