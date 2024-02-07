3814d1899dee03fb2a46a63262bc7c55b1558925
seamus-giess <seamusgiess@gmail.com>
2024-02-08 00:08:38 +1000

    fix: font path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,15 @@
     import "../app.scss";
 </script>
 
+<svelte:head>
+    <style>
+        @font-face {
+            font-family: "DTM-Mono";
+            font-style: normal;
+            font-weight: 500;
+            src: url("fonts/dtm-mono.woff") format("woff");
+        }
+    </style>
+</svelte:head>
+
 <slot />
